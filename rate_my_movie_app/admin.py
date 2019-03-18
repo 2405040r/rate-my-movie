eb9c5e6f243f4fe9ae014c373316e1da9da4eacc
Edward Richards <2405040r@student.gla.ac.uk>
2019-03-18 21:06:56 +0000

    merged genres page, populate_db.py and admin.py changes

--- rate_my_movie_app/admin.py
+++ rate_my_movie_app/admin.py
@@ -5,14 +5,3 @@ admin.site.register(Genre)
 admin.site.register(Movie)
 admin.site.register(Comment)
 admin.site.register(UserProfile)
-
-
-
-from rate_my_movie_app.models import Genre, Movie, UserProfile, Comment
-# Register your models here.
-
-
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(UserProfile)
-admin.site.register(Comment)
