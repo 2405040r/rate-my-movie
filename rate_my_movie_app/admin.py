b86cc97099ce4c71e19762d9b1912b97065be959
Luke Poutney <2314348p@student.gla.ac.uk>
2019-03-18 21:02:19 +0000

    added ability to list genres on web page

--- rate_my_movie_app/admin.py
+++ rate_my_movie_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from rate_my_movie_app.models import Genre, Movie, Comment, UserProfile
+
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Comment)
+admin.site.register(UserProfile)
+
+
 
 # Register your models here.
