7a16caf4aaa908402c4761501da3872d4515cdd1
Luke Poutney <2314348p@student.gla.ac.uk>
2019-03-20 09:33:44 +0000

    most views now in order and added top 5 to home page

--- rate_my_movie_app/views.py
+++ rate_my_movie_app/views.py
@@ -8,14 +8,16 @@ from rate_my_movie_app.forms import MovieForm
 #When the page is requested by the user the corresponding function is called
 
 def home(request):
+	movie_list = Movie.objects.order_by('-views')[:5]
+	context_dict = {'movies': movie_list}
 	#The return statement refers to a html document locates in templates
-	return render(request, 'rate_my_movie_app/home.html')
+	return render(request, 'rate_my_movie_app/home.html', context_dict)
 
 def aboutus(request):
 	return render(request, 'rate_my_movie_app/aboutus.html')
 	
 def mostpopular(request):
-	movie_list = Movie.objects.all() #.order_by('-likes')[:5]
+	movie_list = Movie.objects.order_by('-views').all()
 	context_dict = {'movies': movie_list}
 	
 	return render(request, 'rate_my_movie_app/mostpopular.html', context_dict)
@@ -24,7 +26,7 @@ def rumours(request):
 	return render(request, 'rate_my_movie_app/rumours.html')
 
 def genres(request):
-	genre_list = Genre.objects.all() #.order_by('-likes')[:5]
+	genre_list = Genre.objects.all()
 	context_dict = {'genres': genre_list}
 
 	return render(request, 'rate_my_movie_app/genres.html', context_dict)
